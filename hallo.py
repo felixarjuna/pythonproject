a119404bcaa7560a4bfd2d9badcdaad30338eb83
felixarjuna <felixarjuna.de@yahoo.com>
2021-11-02 21:40:20 +0100

    My Project first commit

---
+++ hallo.py
@@ -0,0 +1,23 @@
+from person import Person
+
+
+def alexMethod(name):
+    p = Person(name)
+    print(p)
+
+
+if __name__ == '__main__':
+    # TODO : Fix Bug
+    # TODO : Implement Tests
+    print("Pycharm is Awesome")
+    print("hello")
+    print("hello")
+    print("Ahmed")
+    alexMethod("Ali")
+
+
+    def hello(name: str):
+        print(name.upper())
+
+    def hello2(name: str):
+        print(name.upper())
